c8c0b0aa6839515792ef1ee7f3b7251ce6f51aa6
toufik <toufik@toufiks-MacBook-Pro.local>
2019-10-11 18:28:07 +0200

    add routing

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DriverListComponent } from './driver-list/driver-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TeamListComponent } from './team-list/team-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'drivers', component: DriverListComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'teams', component: TeamListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { DriverListComponent } from './driver-list/driver-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TeamListComponent } from './team-list/team-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    DriverListComponent,
+    DashboardComponent,
+    TeamListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
-  ],
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
