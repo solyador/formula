505690dd21fb268f5ddb3f160aa35dffdc42b9e6
toufik <toufik@toufiks-MacBook-Pro.local>
2019-10-11 02:32:56 +0200

    add pilot list

---
+++ webapp/src/app/pilot/pilot-list/pilot-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pilot-list',
+    templateUrl: './pilot-list.component.html',
+    styleUrls: ['./pilot-list.component.scss']
+})
+
+export class PilotListComponent {}
