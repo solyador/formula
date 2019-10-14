54a2548b2b4f39501132018e696f1d42f12220d6
toufik <toufik@toufiks-MacBook-Pro.local>
2019-10-14 09:01:39 +0200

    change driver

--- driver-service/src/main/java/org/formula/driver/LoadDataBase.java
+++ driver-service/src/main/java/org/formula/driver/LoadDataBase.java
@@ -12,11 +12,13 @@ public class LoadDataBase {
 
     @Bean
     CommandLineRunner intiDataBase(DriverRepository driverRepository) {
+        Double d = 1.213;
         return args -> {
             log.info("Preloading " + driverRepository.save(new Driver("Louis", "Hamilton", "Mercedes")));
             log.info("Preloading " + driverRepository.save(new Driver("Valteri", "Bottas", "Mercedes")));
             log.info("Preloading " + driverRepository.save(new Driver("Charles", "Leclerc", "Ferrari")));
             log.info("Preloading " + driverRepository.save(new Driver("Sebastien", "Vettel", "Ferrari")));
+
         };
     }
 }
